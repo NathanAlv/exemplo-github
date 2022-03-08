d6777ac1123bf9f547a5edb627ac9f1aac8fcb54
Nathan <nathanalvez10@gmail.com>
2022-03-08 19:46:44 -0400

    commit inicial

---
+++ main.js
@@ -0,0 +1,13 @@
+import './style.css'
+
+const myspan = document.querySelector('span')
+myspan.innerText= 'Eu sou um span'
+
+
+const myH1= document.querySelector('h1')
+myH1.style.color = 'blue'
+myH1.style.fontSize = '300%'
+
+const MyP = document.querySelector('p')
+//MyP.remove()
+MyP.style.display = 'none'
